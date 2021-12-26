5f3dbf8c3887a2683c3c24f52e40e20551b290a4
Mariam Ayoade <92252531+Mayacode00@users.noreply.github.com>
2021-12-26 09:15:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,34 +11,41 @@ switch (new Date().getDay()) {
     mainQuote =
       "“Coming together is a beginning. Keeping together is progress. Working together is success.”";
     author = "Henry Ford";
+    break;
   case 1:
     header = "Quote for Monday";
     mainQuote =
       "“Each morning when I open my eyes I say to myself: I, not events, have the power to make me happy or unhappy today. I can choose which it shall be. Yesterday is dead, tomorrow hasn’t arrived yet. I have just one day, today, and I’m going to be happy in it.”";
     author = "Groucho Marx";
+    break;
   case 2:
     header = "Quote for Tuesday";
     mainQuote = "“A year from now you may wish you had started today.”";
     author = "Karen Lamb";
+    break;
   case 3:
     header = "Quote for Wednesday";
     mainQuote =
       "“You are always stronger and more resourceful than you give yourself credit for.”";
     author = "Rob Moore";
+    break;
   case 4:
     header = "Quote for Thursday";
     mainQuote =
       "“The true meaning of life is to plant trees under whose shade you do not expect to sit.”";
     author = "Nelson Henderson";
+    break;
   case 5:
     header = "Quote for Friday";
     mainQuote =
       "“I have not failed. I’ve just found 10,000 ways that won’t work.”";
     author = "Thomas A. Edison";
+    break;
   case 6:
     header = "Quote for Saturday";
     mainQuote = "“Without the weekend, where would the week be?”";
     author = "Anthony T. Hincks";
+    break;
 }
 
 document.querySelector(".header").innerHTML = header;
